8ccccf66d913fdb60f23a8e60f5640b07361f241
Ramsay <ramsaygonzalez@outlook.com>
2020-08-19 18:26:59 -0400

    added employee tracker schema, created DB in mysql,  and employeetracker.js file to connect to DB.

---
+++ employeeTracker-Schema.sql
@@ -0,0 +1,33 @@
+DROP DATABASE IF EXISTS employeeTrackerDB;
+CREATE database employeeTrackerDB;
+
+USE employeeTrackerDB;
+
+CREATE TABLE departments
+(
+  department_ID INT NOT NULL,
+  name VARCHAR(30) NULL,
+  PRIMARY KEY (department_ID)
+);
+
+CREATE TABLE roles
+(
+  role_ID INT NOT NULL,
+  title VARCHAR(30) NULL,
+  salary DECIMAL(10,4) NULL,
+  department_ID INT NULL,
+  PRIMARY KEY (role_ID),
+  FOREIGN KEY (department_ID) REFERENCES departments(department_ID)
+);
+
+CREATE TABLE employees
+(
+  employee_ID INT NOT NULL,
+  first_name VARCHAR(30) NULL,
+  last_name VARCHAR(30) NULL,
+  role_ID INT NULL,
+  manager_ID INT NULL,
+  PRIMARY KEY (employee_ID),
+  FOREIGN KEY (role_ID) REFERENCES roles(role_ID),
+  FOREIGN KEY (manager_ID) REFERENCES employees(employee_ID)
+);
